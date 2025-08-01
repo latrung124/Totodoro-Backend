50c5f2d1175c6dca8d587dd36ce755de51d8d4ac
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-08-01 21:58:31 +0700

    Implement notification service package

---
+++ internal/notification/service.go
@@ -0,0 +1,57 @@
+/*
+* File: internal/notification/service.go
+* Author: trung.la
+* Date: 01/08/2025
+* Package: github.com/latrung124/Totodoro-Backend/internal/notification
+* Description: This file contains the service layer for notification management.
+ */
+
+package notification
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/latrung124/Totodoro-Backend/internal/database"
+	pb "github.com/latrung124/Totodoro-Backend/internal/proto_package/notification_service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Service struct {
+	pb.UnimplementedNotificationServiceServer
+	db *database.Connections
+}
+
+func NewService(db *database.Connections) *Service {
+	return &Service{db: db}
+}
+
+func (s *Service) CreateNotification(ctx context.Context, req *pb.CreateNotificationRequest) (*pb.CreateNotificationResponse, error) {
+	if req.UserId == "" {
+		return nil, status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	if req.Message == "" {
+		return nil, status.Error(codes.InvalidArgument, "message is required")
+	}
+
+	notificationId := "notification_" + time.Now().Format("20060102150405")
+	newNotification := &pb.Notification{
+		NotificationId: notificationId,
+		UserId:         req.UserId,
+		Message:        req.Message,
+		Type:           req.Type,
+		ScheduledTime:  req.ScheduledTime,
+		Status:         pb.NotificationStatus_SENT,
+	}
+
+	_, err := s.db.NotificationDB.ExecContext(ctx, "INSERT INTO notifications (notification_id, user_id, message, type, scheduled_time, status) VALUES ($1, $2, $3, $4, $5)", newNotification.NotificationId, newNotification.UserId, newNotification.Message, newNotification.ScheduledTime, newNotification.Status)
+	if err != nil {
+		log.Printf("Error inserting notification into database: %v", err)
+		return nil, status.Error(codes.Internal, "failed to create notification")
+	}
+
+	return &pb.CreateNotificationResponse{Notification: newNotification}, nil
+}
